9b6e30762cdd049a9ca47a7e462c34fbf2701617
Maria Markelova <volkova0512@gmail.com>
2020-11-04 00:15:59 -0500

    add data and helpers

---
+++ test/smoke/inputs.js
@@ -0,0 +1,36 @@
+// const sel = require('../../data/selectors.json');
+// const exp = require ('../../data/expected.json');
+//
+// describe('Labels exist', function () {
+//
+//     it('TC-010 Label for image', function () {
+//         const label = $$(sel.image)[4].isDisplayed();
+//         expect(label).toEqual(true);
+//     });
+//
+//     it('TC-011 Label for Create Button', function () {
+//         const label = $$(sel.create)[5].isDisplayed();
+//         expect(label).toEqual(true);
+//     });
+//
+//     it('TC-012 Label for Header', function () {
+//         const label = $('h3').isDisplayed();
+//         expect(label).toEqual(true);
+//     });
+//
+// });
+//
+//
+// // describe('Labels are correct', function () {
+// //
+// //     it('TC-012 Label for image = 5. Upload an image (optional)', function () {
+// //         const text = $$(sel.image)[4].getAttribute('title');
+// //         expect(text).toEqual( exp.labelImage);
+// //     });
+// //
+// //     it('TC-013 Label for Create Button = Create', function () {
+// //         const text = $$(sel.image)[4].getAttribute('title');
+// //         expect(text).toEqual( exp.labelImage);
+// //     });
+// //
+// // });
--- test/smoke/labels.js
+++ test/smoke/labels.js
@@ -40,7 +40,7 @@ describe('My Little Hero', function () { //define suite title by passing a strin
 
     describe('Labels are correct', function () {
 
-        it('TC-006 Label for name = 1. What is your HERO\'s name?', function () {
+        it('TC-006 Label for name = 1. What is your Hero\'s name?', function () {
             const text = $$(sel.label)[0].getAttribute('title');
             expect(text).toEqual( exp.labelName);
         });
@@ -49,16 +49,16 @@ describe('My Little Hero', function () { //define suite title by passing a strin
             const text = $$('.ant-form-item-required')[1].getAttribute('title');
             expect(text).toEqual(exp.labelGender);
         });
-        //
-        // it('TC-004 Label for age', function () {
-        //     const label = $$('.ant-form-item-required')[2].isDisplayed();
-        //     expect(label).toEqual(true);
-        // });
-        //
-        // it('TC-005 Label for story', function () {
-        //     const label = $$('.ant-form-item-required')[3].isDisplayed();
-        //     expect(label).toEqual(true);
-        // });
+
+        it('TC-008 Label for age = 3. How old is your hero?', function () {
+            const text = $$('.ant-form-item-required')[2].getAttribute('title');
+            expect(text).toEqual(exp.labelAge);
+        });
+
+        it('TC-009 Label for story = 4. What type of story would you like to read?', function () {
+            const text = $$('.ant-form-item-required')[3].getAttribute('title');
+            expect(text).toEqual(exp.labelStory);
+        });
 
     });
 
